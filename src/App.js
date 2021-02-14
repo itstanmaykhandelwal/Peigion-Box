0c0fdca122e8941d48f2c1576fb5747878b880d8
Tanmay <iamtanmaykh07@gmail.com>
2021-02-14 17:59:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { ChatEngine} from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const projectID='628ff44c-a9d1-4fdb-b1f7-e0feaeeb1f03'
+
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+    return(
+        // <ChatEngine
+        //     height='100vh'
+        //     projectID='628ff44c-a9d1-4fdb-b1f7-e0feaeeb1f03'
+        //     userName='iamtanmay'
+        //     userSecret='1234'
+        //     renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        // />
+        <ChatEngine
+            height="100vh"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            renderIsTyping={(people) => <ChatFeed {...people} />}
+            onDeleteChat={(chat) => console.log(chat)}
+        />
+    )
 }
 
 export default App;
